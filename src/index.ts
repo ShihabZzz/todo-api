8446055884bd541654bdc3827ccac3e438066a59
ShihabZzz <shihab.riadn@gmail.com>
2024-10-10 18:36:56 +0600

    Improve validation scenarios [1/2]

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,19 @@ interface Todo {
 }
 const db: Record<string, Todo[]> = {};
 
+const findUser = (userID: string) => {
+  return db[userID] || false
+}
+
+const findByTodoID = (db: Todo[], id: string) => {
+  return db.find(todo => todo.id === id) || false
+}
+
 // get all todos
 app.get('/:userID/todos', async (c) => {
   try {
     const user = c.req.param('userID')
-    if (!(`${user}` in db)) {
+    if (!findUser(user)) {
       return c.json({ message: "User not found" }, 404)
     }
     const data = db[user]
@@ -33,7 +41,7 @@ app.get('/:userID/todos/:id', async (c) => {
   try {
     const user = c.req.param('userID')
     const id = c.req.param('id')
-    if (!(`${user}` in db) || db[user].find(todo => todo.id === id) === undefined) {
+    if (!findUser(user) || !findByTodoID(db[user], id)) {
       return c.json({ message: "Todo not found" }, 404)
     }
     const data = db[user].find(todo => todo.id === id)
@@ -48,15 +56,20 @@ app.post('/:userID/todos', async (c) => {
   try {
     const user = c.req.param('userID')
     const body = await c.req.json()
-    if (!body.title) {
+    if (!body.title || body.title.trim() === "") {
       return c.json({ message: "Title is required" }, 400)
-    } else if (body.id || body.status || body.createdAt || body.updatedAt) {
+    }
+    const bodyKeys = Object.keys(body);
+    const hasInvalidkeys = bodyKeys.some((key) => {
+      return key !== "title" && key !== "status"
+    })
+    if (hasInvalidkeys) {
       return c.json({ message: "Invalid request body" }, 400)
     }
     const newTodo: Todo = {
       title: body.title,
       id: crypto.randomUUID(),
-      status: "todo",
+      status: body.status || "todo",
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     }
@@ -64,7 +77,7 @@ app.post('/:userID/todos', async (c) => {
       db[user] = []
     }
     db[user].push(newTodo)
-    return c.json(newTodo, 201)
+    return c.json({ message: "Todo created", newTodo }, 201)
   } catch (error) {
     return c.json({ message: "Internal Server Error" }, 500)
   }
@@ -76,11 +89,18 @@ app.put('/:userID/todos/:id', async (c) => {
     const user = c.req.param('userID')
     const id = c.req.param('id')
     const body = await c.req.json()
-    if (!(`${user}` in db) || db[user].find(todo => todo.id === id) === undefined) {
+    if (!findUser(user) || !findByTodoID(db[user], id)) {
       return c.json({ message: "Todo not found" }, 404)
     }
-    if (body.id || body.createdAt || body.updatedAt) {
+    const bodyKeys = Object.keys(body);
+    const hasInvalidkeys = bodyKeys.some((key) => {
+      return key !== "title" && key !== "status"
+    })
+    if (hasInvalidkeys) {
       return c.json({ message: "Invalid request body" }, 400)
+    }
+    if (!body.title || body.title.trim() === "" || !body.status || body.status.trim() === "") {
+      return c.json({ message: "Title or Status Can't be empty." }, 400)
     }
     const todoIndex = db[user].findIndex(todo => todo.id === id)
     const updatedTodo = {
@@ -89,7 +109,7 @@ app.put('/:userID/todos/:id', async (c) => {
       updatedAt: new Date().toISOString()
     }
     db[user][todoIndex] = updatedTodo
-    return c.json(updatedTodo, 200)
+    return c.json({ message: "Todo updated", updatedTodo }, 200)
   } catch (error) {
     return c.json({ message: "Internal Server Error" }, 500)
   }
@@ -100,7 +120,7 @@ app.delete('/:userID/todos/:id', async (c) => {
   try {
     const user = c.req.param('userID')
     const id = c.req.param('id')
-    if (!(`${user}` in db) || db[user].find(todo => todo.id === id) === undefined) {
+    if (!findUser(user) || !findByTodoID(db[user], id)) {
       return c.json({ message: "Todo not found" }, 404)
     }
     db[user] = db[user].filter(todo => todo.id !== id)
@@ -114,7 +134,7 @@ app.delete('/:userID/todos/:id', async (c) => {
 app.delete('/:userID/todos', async (c) => {
   try {
     const user = c.req.param('userID')
-    if (!(`${user}` in db)) {
+    if (!findUser(user)) {
       return c.json({ message: "User not found" }, 404)
     }
     db[user] = []
