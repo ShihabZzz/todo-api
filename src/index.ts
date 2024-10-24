0f11f807f37da33ad0d89a83aeda4163e41079b1
ShihabZzz <shihab.riadn@gmail.com>
2024-10-25 05:47:36 +0600

    api: move utility functions to separate file

--- src/index.ts
+++ src/index.ts
@@ -2,47 +2,18 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { eq } from "drizzle-orm";
 import { prettyJSON } from "hono/pretty-json";
-import { z } from "zod";
 import { db } from "../connection";
 import { usersTable, todosTable } from "./db/schema";
+import {
+  findUser,
+  findByTodoID,
+  hasInvalidkeys,
+  titleValidation,
+  statusValidation,
+} from "../utils";
 
 const app = new Hono();
 app.use(prettyJSON());
-
-const findUser = async (user: string) => {
-  return await db
-    .select()
-    .from(usersTable)
-    .where(eq(usersTable.user, user))
-    .get();
-};
-
-const findByTodoID = async (id: string) => {
-  return await db.select().from(todosTable).where(eq(todosTable.id, id)).get();
-};
-
-const hasInvalidkeys = (bodyKeys: string[]) => {
-  return bodyKeys.some((key) => {
-    return key !== "title" && key !== "status";
-  });
-};
-
-const titleValidation = z
-  .string({ required_error: "title is required" })
-  .min(3, { message: "title must be at least 3 characters" })
-  .max(100, { message: "title must not exceed 100 characters" })
-  .refine((title) => title.trim() !== "" && isNaN(Number(title)), {
-    message: "title must be valid string type",
-  });
-
-const statusValidation = z
-  .string()
-  .min(3, { message: "status must be at least 3 characters" })
-  .max(50, { message: "status must not exceed 50 characters" })
-  .refine((status) => status.trim() !== "", {
-    message: "status must be valid string type",
-  })
-  .optional();
 
 /* Get_All_Todos */
 app.get("/:user/todos", async (c) => {
